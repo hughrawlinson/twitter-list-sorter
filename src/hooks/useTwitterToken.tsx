0665f470c60ce022f9c0463b36703b4c768e76b7
Hugh Rawlinson <hughr2005@gmail.com>
2022-01-31 10:31:54 +0100

    Update call to createPersistedState for new API

--- src/hooks/useTwitterToken.tsx
+++ src/hooks/useTwitterToken.tsx
@@ -1,9 +1,11 @@
 import createPersistedState from "use-persisted-state";
 
-const useTwitterTokenState = createPersistedState("TwitterToken");
+const useTwitterTokenState = createPersistedState<string | null>(
+  "TwitterToken"
+);
 
 export const useTwitterToken = (): string | null => {
-  const [token, setToken] = useTwitterTokenState<string | null>(null);
+  const [token, setToken] = useTwitterTokenState(null);
 
   if (typeof window === "undefined") {
     // we're on the nextjs server, no twitter auth for us!
